bbc82b53b73b415d8b5e5007f90d80f8085abcc6
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-05-24 22:34:33 -0300

    -Details of the column annotation

--- src/main/java/com/ricardomaricato/ecommerceproject/model/Produto.java
+++ src/main/java/com/ricardomaricato/ecommerceproject/model/Produto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -20,6 +21,12 @@ public class Produto {
     @Id @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "data_criacao", updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_ultima_atualizacao", insertable = false)
+    private LocalDateTime dataUltimaAtualizacao;
 
     private String nome;
 
